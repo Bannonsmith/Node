38c33ee99d95de5fea24dd4f0f6cf90f2fdd158a
Bannonsmith <Bannonsmith13@gmail.com>
2019-07-14 23:12:52 -0500

    insert item into database

--- node-pgpromise/app.js
+++ node-pgpromise/app.js
@@ -1,7 +1,19 @@
 
+
+// Initialize the database
 const pgp =  require("pg-promise")()
 const connectionString = "postgres://localhost:5432/edensgarden"
 const db = pgp(connectionString)
 
+// Insert item into the database without returning anything
+db.none("INSERT INTO dishes(name,course,price,imageURL) VALUES($1,$2,$3,$4)", ["Chicken Sandwich","Entrees",6.50,"chickensandwich.png"])
+.then(() => {
+    console.log("SUCCESS")
+}).catch(error => console.log(error))
 
-console.log(db)
+// Insert item and return the primary key in the console log
+
+db.one("INSERT INTO dishes(name,course,price,imageURL) VALUES($1,$2,$3,$4)  RETURNING dishid", ["Egg Drop","Starter",3.70,"soup.png"])
+.then((data) => {
+    console.log(data.dishid)
+}).catch(error => console.log(error))
