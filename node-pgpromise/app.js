109334b692134c1110815415678f683fde03fe9b
Bannonsmith <Bannonsmith13@gmail.com>
2019-07-14 23:36:32 -0500

    update using pg promise in database

--- node-pgpromise/app.js
+++ node-pgpromise/app.js
@@ -8,14 +8,34 @@ const db = pgp(connectionString)
 
 // CRUD operations
 
-// using placeholders in  your seach request (more dynamic)
-db.any("SELECT name,course,price,imageurl FROM dishes WHERE price > $1;",[5])
-.then((dishes) => {
-    console.log(dishes)
+// update specific item using dishid
+db.none("UPDATE dishes SET price = $1, course = $2 WHERE dishid = $3",[15,"Entrees",8])
+.then(() => {
+    console.log("UPDATED")
 }).catch(error => console.log(error))
 
 
+
+// updates just the price using the dish id for the particular item
+
+
+// db.none("UPDATE dishes SET price = $1 WHERE dishid = $2",[11,8])
+// .then(() => {
+//     console.log("UPDATED")
+// }).catch(error => console.log(error))
+
+// using placeholders in  your seach request (more dynamic)
+
+
+// db.any("SELECT name,course,price,imageurl FROM dishes WHERE price > $1;",[5])
+// .then((dishes) => {
+//     console.log(dishes)
+// }).catch(error => console.log(error))
+
+
 // add specifics to your search 
+
+
 // db.any("SELECT name,course,price,imageurl FROM dishes WHERE price > 5;")
 // .then((dishes) => {
 //     console.log(dishes)
@@ -24,12 +44,16 @@ db.any("SELECT name,course,price,imageurl FROM dishes WHERE price > $1;",[5])
 
 
 // retrieve all items
+
+
 // db.any("SELECT name,course,price,imageurl  FROM  dishes;")
 // .then((dishes) => {
 //     console.log(dishes)
 // }).catch(error => console.log(error))
 
 // Insert item into the database without returning anything
+
+
 // db.none("INSERT INTO dishes(name,course,price,imageURL) VALUES($1,$2,$3,$4)", ["Chicken Sandwich","Entrees",6.50,"chickensandwich.png"])
 // .then(() => {
 //     console.log("SUCCESS")
